305f3ca5d1589e30d4ff10d0d6f9b00ba3ee387f
Elenterius <elenterius@users.noreply.github.com>
2020-07-21 23:18:24 +0200

    implements iGrowable for SugarCaneBlock

--- src/api/java/com/creativechasm/environment/mixin/MixinSugarCaneBlock.java
+++ src/api/java/com/creativechasm/environment/mixin/MixinSugarCaneBlock.java
@@ -3,8 +3,8 @@ package com.creativechasm.environment.mixin;
 import com.creativechasm.environment.api.block.BlockPropertyUtil;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.CactusBlock;
 import net.minecraft.block.IGrowable;
+import net.minecraft.block.SugarCaneBlock;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
@@ -18,13 +18,13 @@ import org.spongepowered.asm.mixin.Shadow;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Random;
 
-@Mixin(CactusBlock.class)
-public class MixinCactusBlock extends Block implements IGrowable
+@Mixin(SugarCaneBlock.class)
+public class MixinSugarCaneBlock extends Block implements IGrowable
 {
     @Shadow @Final public static IntegerProperty AGE;
     public final int MAX_AGE = BlockPropertyUtil.getMaxAge(AGE);
 
-    public MixinCactusBlock(Properties properties) {
+    public MixinSugarCaneBlock(Properties properties) {
         super(properties);
     }
 
@@ -34,12 +34,12 @@ public class MixinCactusBlock extends Block implements IGrowable
         BlockPos upPos = pos.up();
         BlockState upState = worldIn.getBlockState(upPos);
         if (upState.isAir(worldIn, upPos)) {
-            int n = getNumOfCactusBlocksBelow(worldIn, pos);
+            int n = getNumOfSugarCaneBlocksBelow(worldIn, pos);
             return n + 1 < 3 && worldIn.getBlockState(pos).get(AGE) < MAX_AGE;
         }
         else if (upState.getBlock() == this) {
-            int nUp = getNumOfCactusBlocksAbove(worldIn, pos);
-            int nDown = getNumOfCactusBlocksBelow(worldIn, pos);
+            int nUp = getNumOfSugarCaneBlocksAbove(worldIn, pos);
+            int nDown = getNumOfSugarCaneBlocksBelow(worldIn, pos);
             return nUp + nDown + 1 < 3 & worldIn.getBlockState(pos.up(nUp)).get(AGE) < MAX_AGE;
         }
         return false;
@@ -60,20 +60,20 @@ public class MixinCactusBlock extends Block implements IGrowable
             grow(worldIn, pos, state, rand);
         }
         else {
-            int nUp = getNumOfCactusBlocksAbove(worldIn, pos);
+            int nUp = getNumOfSugarCaneBlocksAbove(worldIn, pos);
             BlockPos topPos = pos.up(nUp);
             BlockState topState = worldIn.getBlockState(topPos);
             grow(worldIn, topPos, topState, rand);
         }
     }
 
-    protected int getNumOfCactusBlocksAbove(IBlockReader worldIn, BlockPos pos) {
+    protected int getNumOfSugarCaneBlocksAbove(IBlockReader worldIn, BlockPos pos) {
         int i = 0;
         while (i < 3 && worldIn.getBlockState(pos.up(i + 1)).getBlock() == this) i++;
         return i;
     }
 
-    protected int getNumOfCactusBlocksBelow(IBlockReader worldIn, BlockPos pos) {
+    protected int getNumOfSugarCaneBlocksBelow(IBlockReader worldIn, BlockPos pos) {
         int i = 0;
         while (i < 3 && worldIn.getBlockState(pos.down(i + 1)).getBlock() == this) i++;
         return i;
@@ -84,10 +84,7 @@ public class MixinCactusBlock extends Block implements IGrowable
         if (age == MAX_AGE) {
             BlockPos upPos = pos.up();
             worldIn.setBlockState(upPos, getDefaultState());
-
-            BlockState newState = state.with(AGE, 0);
-            worldIn.setBlockState(pos, newState, Constants.BlockFlags.NO_RERENDER);
-            newState.neighborChanged(worldIn, upPos, this, pos, false);
+            worldIn.setBlockState(pos, state.with(AGE, 0), Constants.BlockFlags.NO_RERENDER);
         }
         else {
             worldIn.setBlockState(pos, state.with(AGE, age), Constants.BlockFlags.NO_RERENDER);
