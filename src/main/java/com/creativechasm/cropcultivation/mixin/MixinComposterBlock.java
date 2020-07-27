1b93fc16e39d9025f5eddbcb9b000b4f07d2f4df
Elenterius <elenterius@users.noreply.github.com>
2020-07-27 18:39:27 +0200

    refactors mixins - removed reliance on forge AT for composter mixin to access nested class - stonecutter: check the crafted item is of rock material

--- src/main/java/com/creativechasm/cropcultivation/mixin/MixinComposterBlock.java
+++ src/main/java/com/creativechasm/cropcultivation/mixin/MixinComposterBlock.java
@@ -4,22 +4,27 @@ import com.creativechasm.cropcultivation.api.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ComposterBlock;
-import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.util.Direction;
+import net.minecraft.util.IItemProvider;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nonnull;
 import java.util.Random;
@@ -29,76 +34,77 @@ import java.util.Random;
  * - nerf speed<br>
  * - return compost instead of bone meal
  */
-@Mixin(ComposterBlock.class)
-public abstract class MixinComposterBlock extends Block {
+public abstract class MixinComposterBlock
+{
+    @Mixin(ComposterBlock.class)
+    public static abstract class Composter extends Block
+    {
+        @Shadow @Final public static IntegerProperty LEVEL;
 
-    @Shadow @Final public static IntegerProperty LEVEL;
+        public Composter(Properties properties) {
+            super(properties);
+        }
 
-    public MixinComposterBlock(Properties properties) {
-        super(properties);
-    }
+        @Redirect(method = "onBlockActivated", at = @At(value = "NEW", target = "net/minecraft/item/ItemStack"))
+        protected ItemStack redirectSpawnedItem(IItemProvider item) {
+            return new ItemStack(ModItems.COMPOST, 3); //replaces bone meal output
+        }
 
-//    @Redirect(
-//            method = "onBlockActivated",
-//            at = @At(value = "FIELD", target = "Lnet/minecraft/item/Items;BONE_MEAL:Lnet/minecraft/item/Item;", opcode = Opcodes.GETSTATIC)
-//    )
-//    protected Item redirectSpawnedItem() {
-//        return LibItems.COMPOST; //replaces bone meal output
-//    }
+        @Redirect(method = "createInventory", at = @At(value = "NEW", target = "net/minecraft/item/ItemStack"))
+        protected ItemStack redirectInventoryItem(IItemProvider item) {
+            return new ItemStack(ModItems.COMPOST, 3); //replaces bone meal output
+        }
 
-    @Redirect(
-            method = "onBlockActivated",
-            at = @At(value = "NEW", target = "net/minecraft/entity/item/ItemEntity")
-    )
-    protected ItemEntity redirectSpawnedItem(World world, double x, double y, double z, ItemStack stack) {
-        return new ItemEntity(world, x, y, z, new ItemStack(ModItems.COMPOST, 3)); //replaces bone meal output
-    }
+        @ModifyArg(
+                method = "onBlockAdded",
+                at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ITickList;scheduleTick(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"),
+                index = 2
+        )
+        protected int adjustOnBlockAddedTicks(int ticks) {
+            return ticks * 6; //increases time needed to convert material to compost
+        }
 
-//    @Redirect(
-//            method = "createInventory",
-//            at = @At(value = "FIELD", target = "Lnet/minecraft/item/Items;BONE_MEAL:Lnet/minecraft/item/Item;", opcode = Opcodes.GETSTATIC)
-//    )
-//    protected Item redirectInventoryItem() {
-//        return LibItems.COMPOST; //replaces bone meal output
-//    }
+        @ModifyArg(
+                method = "addItem",
+                at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ITickList;scheduleTick(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"),
+                index = 2
+        )
+        private static int adjustAddItemTicks(int ticks) {
+            return ticks * 6; //increases time needed to convert material to compost
+        }
 
-    @Redirect(
-            method = "createInventory",
-            at = @At(value = "NEW", target = "net/minecraft/block/ComposterBlock$FullInventory") //target reference is correct
-    )
-    protected ComposterBlock.FullInventory redirectInventoryItem(BlockState state, IWorld iWorld, BlockPos pos, ItemStack stack) {
-        return new ComposterBlock.FullInventory(state, iWorld, pos, new ItemStack(ModItems.COMPOST, 3)); //replaces bone meal output
-    }
-
-    @ModifyArg(
-            method = "onBlockAdded",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ITickList;scheduleTick(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"),
-            index = 2
-    )
-    protected int adjustOnBlockAddedTicks(int ticks) {
-        return ticks * 6; //increases time needed to convert material to compost
-    }
-
-    @ModifyArg(
-            method = "addItem",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ITickList;scheduleTick(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"),
-            index = 2
-    )
-    private static int adjustAddItemTicks(int ticks) {
-        return ticks * 6; //increases time needed to convert material to compost
-    }
-
-    @Override
-    @OnlyIn(Dist.CLIENT)
-    public void animateTick(@Nonnull BlockState stateIn, @Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull Random rand) {
-        if (rand.nextInt(5) == 0) {
-            if (stateIn.get(LEVEL) > 2) {
-                BlockState blockstate = worldIn.getBlockState(pos);
-                double yOffset = blockstate.getShape(worldIn, pos).max(Direction.Axis.Y, 0.5D, 0.5D) + 0.03125D;
-                for (int i = 0; i < rand.nextInt(stateIn.get(LEVEL)) + 2; ++i) {
-                    worldIn.addParticle(ParticleTypes.ENTITY_EFFECT, pos.getX() + 0.13125F + 0.7375F * rand.nextFloat(), pos.getY() + yOffset + (1d - yOffset), pos.getZ() + 0.13125F + 0.7375F * rand.nextFloat(), 1.8f, 1.8f, 1.8f);
+        @Override
+        @OnlyIn(Dist.CLIENT)
+        public void animateTick(@Nonnull BlockState stateIn, @Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull Random rand) {
+            if (rand.nextInt(5) == 0) {
+                if (stateIn.get(LEVEL) > 2) {
+                    BlockState blockstate = worldIn.getBlockState(pos);
+                    double yOffset = blockstate.getShape(worldIn, pos).max(Direction.Axis.Y, 0.5D, 0.5D) + 0.03125D;
+                    for (int i = 0; i < rand.nextInt(stateIn.get(LEVEL)) + 2; ++i) {
+                        worldIn.addParticle(ParticleTypes.ENTITY_EFFECT, pos.getX() + 0.13125F + 0.7375F * rand.nextFloat(), pos.getY() + yOffset + (1d - yOffset), pos.getZ() + 0.13125F + 0.7375F * rand.nextFloat(), 1.8f, 1.8f, 1.8f);
+                    }
                 }
             }
+        }
+    }
+
+    @Mixin(targets = "net.minecraft.block.ComposterBlock$FullInventory")
+    public static abstract class FullInventory extends Inventory implements ISidedInventory
+    {
+        @Redirect(method = "canExtractItem", at = @At(value = "FIELD", target = "Lnet/minecraft/item/Items;BONE_MEAL:Lnet/minecraft/item/Item;", opcode = Opcodes.GETSTATIC))
+        protected Item redirectCanExtractItem() {
+            return ModItems.COMPOST; //replaces bone meal
+        }
+
+        @Inject(method = "getInventoryStackLimit", at = @At("HEAD"), cancellable = true)
+        protected void onGetInventoryStackLimit(CallbackInfoReturnable<Integer> cir) {
+            cir.setReturnValue(3);
+        }
+
+        @Override
+        @Nonnull
+        public ItemStack decrStackSize(int index, int count) {
+            return super.decrStackSize(index, 3);
         }
     }
 }
--- src/main/java/com/creativechasm/cropcultivation/mixin/MixinStonecutterContainer.java
+++ src/main/java/com/creativechasm/cropcultivation/mixin/MixinStonecutterContainer.java
@@ -2,9 +2,13 @@ package com.creativechasm.cropcultivation.mixin;
 
 import com.creativechasm.cropcultivation.api.item.ModItems;
 import com.creativechasm.cropcultivation.handler.CraftingHandler;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.container.Slot;
 import net.minecraft.inventory.container.StonecutterContainer;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IWorldPosCallable;
 import org.spongepowered.asm.mixin.Final;
@@ -13,6 +17,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.annotation.Nonnull;
 
 public abstract class MixinStonecutterContainer
 {
@@ -28,31 +34,45 @@ public abstract class MixinStonecutterContainer
     }
 
     @Mixin(targets = "net.minecraft.inventory.container.StonecutterContainer$2")
-    public static abstract class OutputSlot {
+    public static abstract class OutputSlot extends Slot
+    {
         @Shadow(aliases = {"this$0"})
         StonecutterContainer outerClassRef;
 
-        /**
-         * @param stack normally is the crafted item, but when using shift click to craft several items at once this is an empty stack, method is still called for each item created
-         */
-        @Inject(
-                method = "onTake",
-                at = @At("TAIL")
-        )
-        protected void injectOnTake(PlayerEntity player, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
-            if (outerClassRef instanceof CraftingHandler.IWorldPosCallableProvider) {
-                //CraftingHandler.firePlayerStoneCutEvent(player, stack.copy(), ((CraftingHandler.IWorldPosCallableProvider) outerClassRef).getWorldPosCallable());
+        public OutputSlot(IInventory inventoryIn, int index, int xPosition, int yPosition) {
+            super(inventoryIn, index, xPosition, yPosition);
+        }
 
-                ((CraftingHandler.IWorldPosCallableProvider) outerClassRef).getWorldPosCallable().consume((world, pos) -> {
-                    if (!world.isRemote && world.rand.nextFloat() < 0.15f) {
-                        double d0 = world.rand.nextFloat() * 0.5F + 0.15F;
-                        double d1 = world.rand.nextFloat() * 0.5F + 0.5D;
-                        double d2 = world.rand.nextFloat() * 0.5F + 0.15F;
-                        ItemEntity itementity = new ItemEntity(world, pos.getX() + d0, pos.getY() + d1, pos.getZ() + d2, new ItemStack(ModItems.LIME_DUST, world.rand.nextInt(2) + 1));
-                        itementity.setDefaultPickupDelay();
-                        world.addEntity(itementity);
-                    }
-                });
+        @Inject(method = "onTake", at = @At("HEAD"))
+        protected void injectOnTake(PlayerEntity player, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
+            //normally the stack is the crafted item, but when using shift click to craft several items at once this is an empty stack
+            onCraftingCallback(stack);
+        }
+
+        @Override
+        protected void onCrafting(@Nonnull ItemStack stack, int amount) {
+            onCraftingCallback(stack);
+            super.onCrafting(stack, amount);
+        }
+
+        protected void onCraftingCallback(@Nonnull ItemStack stack) {
+            if (!stack.isEmpty()) {
+                //guard against mods adding non-rock recipes
+                if (stack.getItem() instanceof BlockItem && ((BlockItem) stack.getItem()).getBlock().getDefaultState().getMaterial() != Material.ROCK) return;
+
+                //CraftingHandler.firePlayerStoneCutEvent(player, stack.copy(), ((CraftingHandler.IWorldPosCallableProvider) outerClassRef).getWorldPosCallable());
+                if(outerClassRef instanceof CraftingHandler.IWorldPosCallableProvider) {
+                    ((CraftingHandler.IWorldPosCallableProvider) outerClassRef).getWorldPosCallable().consume((world, pos) -> {
+                        if (!world.isRemote && world.rand.nextFloat() < 0.15f) {
+                            double d0 = world.rand.nextFloat() * 0.5F + 0.15F;
+                            double d1 = world.rand.nextFloat() * 0.5F + 0.5D;
+                            double d2 = world.rand.nextFloat() * 0.5F + 0.15F;
+                            ItemEntity itementity = new ItemEntity(world, pos.getX() + d0, pos.getY() + d1, pos.getZ() + d2, new ItemStack(ModItems.LIME_DUST, world.rand.nextInt(stack.getCount()) + 1));
+                            itementity.setDefaultPickupDelay();
+                            world.addEntity(itementity);
+                        }
+                    });
+                }
             }
         }
     }
