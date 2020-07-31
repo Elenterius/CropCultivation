aa79320c91961471c6bc86b1fc1b96a006c7f43b
Elenterius <elenterius@users.noreply.github.com>
2020-07-31 19:42:48 +0200

    fixes stonecutter mixin

--- src/main/java/com/creativechasm/cropcultivation/mixin/MixinStonecutterContainer.java
+++ src/main/java/com/creativechasm/cropcultivation/mixin/MixinStonecutterContainer.java
@@ -1,16 +1,17 @@
 package com.creativechasm.cropcultivation.mixin;
 
+import com.creativechasm.cropcultivation.CropCultivationMod;
 import com.creativechasm.cropcultivation.api.item.ModItems;
-import com.creativechasm.cropcultivation.handler.CraftingHandler;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.Slot;
-import net.minecraft.inventory.container.StonecutterContainer;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IWorldPosCallable;
+import net.minecraft.util.ResourceLocation;
+import org.apache.logging.log4j.MarkerManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,25 +20,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nonnull;
+import java.util.Optional;
 
 public abstract class MixinStonecutterContainer
 {
-    @Mixin(StonecutterContainer.class)
-    public static abstract class Container implements CraftingHandler.IWorldPosCallableProvider
-    {
-        @Shadow @Final private IWorldPosCallable worldPosCallable;
-
-        @Override
-        public IWorldPosCallable getWorldPosCallable() {
-            return worldPosCallable;
-        }
-    }
-
     @Mixin(targets = "net.minecraft.inventory.container.StonecutterContainer$2")
     public static abstract class OutputSlot extends Slot
     {
-        @Shadow(aliases = {"this$0"}, remap = false) //TODO: find workaround for this
-        StonecutterContainer outerClassRef;
+        //        @Shadow(aliases = {"field_216955_a", "this$0"}) @Final private StonecutterContainer field_216955_a;
+        @Shadow(aliases = {"field_216956_b", "val$worldPosCallableIn"}) @Final private IWorldPosCallable field_216956_b;
 
         public OutputSlot(IInventory inventoryIn, int index, int xPosition, int yPosition) {
             super(inventoryIn, index, xPosition, yPosition);
@@ -46,33 +37,41 @@ public abstract class MixinStonecutterContainer
         @Inject(method = "onTake(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;", at = @At("HEAD"))
         protected void injectOnTake(PlayerEntity player, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
             //normally the stack is the crafted item, but when using shift click to craft several items at once this is an empty stack
-            onCraftingCallback(stack);
+            onItemCrafted(stack);
         }
 
         @Override
         protected void onCrafting(@Nonnull ItemStack stack, int amount) {
-            onCraftingCallback(stack);
             super.onCrafting(stack, amount);
+            onItemCrafted(stack);
         }
 
-        protected void onCraftingCallback(@Nonnull ItemStack stack) {
+        protected void onItemCrafted(@Nonnull ItemStack stack) {
             if (!stack.isEmpty()) {
                 //guard against mods adding non-rock recipes
                 if (stack.getItem() instanceof BlockItem && ((BlockItem) stack.getItem()).getBlock().getDefaultState().getMaterial() != Material.ROCK) return;
 
+                //we only want rock that contains trace elements, filters out Stone/Cobblestone
+                String registryName = Optional.ofNullable(stack.getItem().getRegistryName()).map(ResourceLocation::getPath).orElse("");
+                if (!registryName.contains("granite") || !registryName.contains("diorite") || !registryName.contains("andesite")) return;
+
+                //forge is missing a crafting event for the stonecutter :(
+                //could fire custom event, but we don't need it
                 //CraftingHandler.firePlayerStoneCutEvent(player, stack.copy(), ((CraftingHandler.IWorldPosCallableProvider) outerClassRef).getWorldPosCallable());
-                if(outerClassRef instanceof CraftingHandler.IWorldPosCallableProvider) {
-                    ((CraftingHandler.IWorldPosCallableProvider) outerClassRef).getWorldPosCallable().consume((world, pos) -> {
-                        if (!world.isRemote && world.rand.nextFloat() < 0.15f) {
-                            double d0 = world.rand.nextFloat() * 0.5F + 0.15F;
-                            double d1 = world.rand.nextFloat() * 0.5F + 0.5D;
-                            double d2 = world.rand.nextFloat() * 0.5F + 0.15F;
-                            ItemEntity itementity = new ItemEntity(world, pos.getX() + d0, pos.getY() + d1, pos.getZ() + d2, new ItemStack(ModItems.LIME_DUST, world.rand.nextInt(stack.getCount()) + 1));
-                            itementity.setDefaultPickupDelay();
-                            world.addEntity(itementity);
-                        }
-                    });
-                }
+
+                field_216956_b.consume((world, pos) -> {
+                    if (!world.isRemote && world.rand.nextFloat() < 0.15f) {
+                        double d0 = world.rand.nextFloat() * 0.5F + 0.15F;
+                        double d1 = world.rand.nextFloat() * 0.5F + 0.5D;
+                        double d2 = world.rand.nextFloat() * 0.5F + 0.15F;
+
+                        ItemStack byproductStack = new ItemStack(ModItems.LIME_DUST, world.rand.nextInt(stack.getCount()) + 1);
+                        CropCultivationMod.LOGGER.debug(MarkerManager.getMarker("StoneCutter"), "dropping: " + byproductStack);
+                        ItemEntity itementity = new ItemEntity(world, pos.getX() + d0, pos.getY() + d1, pos.getZ() + d2, byproductStack);
+                        itementity.setDefaultPickupDelay();
+                        world.addEntity(itementity);
+                    }
+                });
             }
         }
     }
